6d5edb7b3418d914e0f93f747ce43284d6c9fe1a
chuongtang <chuong.tang@aol.com>
2022-07-24 16:41:16 -0600

    ✔ Successfully added type for MemberVote

--- src/MemberVote.tsx
+++ src/MemberVote.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { useVote } from '@thirdweb-dev/react';
-
+import { useToken, useVote } from '@thirdweb-dev/react';
+import { AddressZero } from "@ethersproject/constants";
 
 
 interface Props {
@@ -10,9 +10,18 @@ interface Props {
   address: string;
 }
 
-const MemberVote = ({ deployedContract, deployedToken, hasClaimedNFT, address } : Props) => {
+interface Vote {
+  type: number;
+  label: string;
+  count: number;
 
+}
+
+const MemberVote = ({ deployedContract, deployedToken, hasClaimedNFT, address }: Props) => {
+  // Initialize our vote contract
   const vote = useVote("0x9393fb107aA2BD1BAe191E82422BC2Fc519fB728")!;
+  // Initialize our token contract
+  const token = useToken("deployedToken")!;
 
   const [proposals, setProposals] = useState<any[]>([]);
   const [isVoting, setIsVoting] = useState<boolean>(false);
@@ -65,11 +74,136 @@ const MemberVote = ({ deployedContract, deployedToken, hasClaimedNFT, address } 
     checkIfUserHasVoted();
 
   }, [hasClaimedNFT, proposals, address, vote]);
+
+  const voteProposal:React.FormEventHandler<HTMLElement>  = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    //before we do async things, we want to disable the button to prevent double clicks
+    setIsVoting(true);
+
+    // lets get the votes from the form for the values
+    const votes = proposals.map((proposal) => {
+      const voteResult = {
+        proposalId: proposal.proposalId,
+        //abstain by default
+        vote: 2,
+      };
+      proposal.votes.forEach((vote: Vote) => {
+        const elem = document.getElementById(
+          proposal.proposalId + "-" + vote.type
+        ) as HTMLInputElement;
+
+        if (elem.checked) {
+          voteResult.vote = vote.type;
+          return;
+        }
+      });
+      return voteResult;
+    });
+
+    // first we need to make sure the user delegates their token to vote
+    try {
+      //we'll check if the wallet still needs to delegate their tokens before they can vote
+      const delegation = await token.getDelegationOf(address);
+      // if the delegation is the 0x0 address that means they have not delegated their governance tokens yet
+      if (delegation === AddressZero) {
+        //if they haven't delegated their tokens yet, we'll have them delegate them before voting
+        await token.delegateTo(address);
+      }
+      // then we need to vote on the proposals
+      try {
+        await Promise.all(
+          votes.map(async ({ proposalId, vote: _vote }) => {
+            // before voting we first need to check whether the proposal is open for voting
+            // we first need to get the latest state of the proposal
+            const proposal = await vote.get(proposalId);
+            // then we check if the proposal is open for voting (state === 1 means it is open)
+            if (proposal.state === 1) {
+              // if it is open for voting, we'll vote on it
+              return vote.vote(proposalId, _vote);
+            }
+            // if the proposal is not open for voting we just return nothing, letting us continue
+            return;
+          })
+        );
+        try {
+          // if any of the propsals are ready to be executed we'll need to execute them
+          // a proposal is ready to be executed if it is in state 4
+          await Promise.all(
+            votes.map(async ({ proposalId }) => {
+              // we'll first get the latest state of the proposal again, since we may have just voted before
+              const proposal = await vote.get(proposalId);
+
+              //if the state is in state 4 (meaning that it is ready to be executed), we'll execute the proposal
+              if (proposal.state === 4) {
+                return vote.execute(proposalId);
+              }
+            })
+          );
+          // if we get here that means we successfully voted, so let's set the "hasVoted" state to true
+          setHasVoted(true);
+          // and log out a success message
+          console.log("successfully voted");
+        } catch (err) {
+          console.error("failed to execute votes", err);
+        }
+      } catch (err) {
+        console.error("failed to vote", err);
+      }
+    } catch (err) {
+      console.error("failed to delegate tokens");
+    } finally {
+      // in *either* case we need to set the isVoting state to false to enable the button again
+      setIsVoting(false);
+    }
+  }
+
+
   return (
     <div>
       <h1>{deployedContract}</h1>
       <h1>{deployedToken}</h1>
-      <h1>{!hasClaimedNFT} This is </h1>
+      <h1>{address} This is addressss </h1>
+      <h2>Active Proposals</h2>
+      <form
+        onSubmit={voteProposal} >
+        {proposals.map((proposal) => (
+          <div key={proposal.proposalId} className="card">
+            <h5>{proposal.description}</h5>
+            <div>
+              {proposal.votes.map(({ type, label }:Vote) => (
+                <div key={type}>
+                  <input
+                    type="radio"
+                    id={proposal.proposalId + "-" + type}
+                    name={proposal.proposalId}
+                    value={type}
+                    //default the "abstain" vote to checked
+                    defaultChecked={type === 2}
+                  />
+                  <label htmlFor={proposal.proposalId + "-" + type}>
+                    {label}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <button disabled={isVoting || hasVoted} type="submit">
+          {isVoting
+            ? "Voting..."
+            : hasVoted
+              ? "You Already Voted"
+              : "Submit Votes"}
+        </button>
+        {!hasVoted && (
+          <small>
+            This will trigger multiple transactions that you will need to
+            sign.
+          </small>
+        )}
+      </form>
     </div>
   )
 }
